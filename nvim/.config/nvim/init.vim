da52327924902403272bdd57b948e7a5d5207aaf
Demes Toss <demestoss@proton.me>
2023-10-05 20:29:29 +0200

    Added tmux config updates

---
+++ nvim/.config/nvim/init.vim
@@ -0,0 +1,4 @@
+lua require('config')
+
+nnoremap <leader>x :!chmod +x %<CR>
+nnoremap <silent> <C-j> :silent !tmux neww tt<CR>
--- nvim/.config/nvim/lua/config.lua
+++ nvim/.config/nvim/lua/config.lua
@@ -21,3 +21,4 @@ require("lazy").setup({{
     "aserowy/tmux.nvim",
     config = function() return require("tmux").setup() end
 }})
+
