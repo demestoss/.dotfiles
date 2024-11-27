b8d340ebbf6f82df3cbb44649c96edfba8698632
Dmitriy Popov <me@demestoss.com>
2024-11-27 20:03:51 +0100

    Removed lazyvim

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-local map = LazyVim.safe_keymap_set
-
-map("i", "jk", "<Esc>", { desc = "Normal mode" })
