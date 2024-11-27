535a581aaa843e251d36cba4d227afe56c1db48f
Dmitriy Popov <me@demestoss.com>
2024-11-27 20:02:14 +0100

    Added lazyvim submodule

---
+++ lazyvim/.config/lazyvim/lua/plugins/extend-gitsigns.lua
@@ -0,0 +1,40 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  keys = {
+    {
+      "<leader>hb",
+      "<cmd>Gitsigns blame_line<cr>",
+      desc = "Blame Line"
+    },
+    {
+      "<leader>hs",
+      "<cmd>Gitsigns stage_hunk<cr>",
+      desc = "Stage Hunk"
+    },
+    {
+      "<leader>hS",
+      "<cmd>Gitsigns stage_buffer<cr>",
+      desc = "Stage Buffer"
+    },
+    {
+      "<leader>hr",
+      "<cmd>Gitsigns reset_hunk<cr>",
+      desc = "Reset Hunk"
+    },
+    {
+      "<leader>hR",
+      "<cmd>Gitsigns reset_buffer<cr>",
+      desc = "Reset Buffer"
+    },
+    {
+      "<leader>hu",
+      "<cmd>Gitsigns undo_stage_hunk<cr>",
+      desc = "Undo Stage Hunk"
+    },
+    {
+      "<leader>hp",
+      "<cmd>Gitsigns preview_hunk<cr>",
+      desc = "Preview Hunk"
+    },
+  },
+}
