632843a39ccc5acc92a862a94ad8f267ff31baac
Dmitriy Popov <me@demestoss.com>
2024-11-28 21:48:27 +0100

    Added new panes keymaps for wez

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -86,7 +86,6 @@ config.keys = {
 
   -- Leader keys
   { key = "[", mods = "LEADER", action = act.ActivateCopyMode },
-  { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
 
   -- Tab keys
   { key = "q", mods = "CMD", action = act.ShowTabNavigator },
@@ -110,21 +109,22 @@ config.keys = {
   -- Pane keys
   { key = "x", mods = "CMD", action = act.CloseCurrentPane({ confirm = false }) },
   {
-    key = "|",
-    mods = "OPT|SHIFT",
+    key = "\\",
+    mods = "CMD",
     action = act.SplitPane({
       direction = "Right",
       size = { Percent = 50 },
     }),
   },
   {
-    key = "-",
-    mods = "OPT",
+    key = "'",
+    mods = "CMD",
     action = act.SplitPane({
       direction = "Down",
       size = { Percent = 50 },
     }),
   },
+  { key = "z", mods = "CMD", action = act.TogglePaneZoomState },
   { key = "{", mods = "CMD|SHIFT", action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }) },
   { key = ";", mods = "CMD", action = act.ActivatePaneDirection("Prev") },
   { key = "o", mods = "CMD", action = act.ActivatePaneDirection("Next") },
@@ -210,18 +210,11 @@ end)
 local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 
 smart_splits.apply_to_config(config, {
-  -- the default config is here, if you'd like to use the default keys,
-  -- you can omit this configuration table parameter and just use
-  -- smart_splits.apply_to_config(config)
-
-  -- directional keys to use in order of: left, down, up, right
   direction_keys = { "h", "j", "k", "l" },
-  -- if you want to use separate direction keys for move vs. resize, you
-  -- can also do this:
-  direction_keys = {
-    move = { "h", "j", "k", "l" },
-    resize = { "LeftArrow", "DownArrow", "UpArrow", "RightArrow" },
-  },
+  -- direction_keys = {
+  --   move = { "h", "j", "k", "l" },
+  --   resize = { "LeftArrow", "DownArrow", "UpArrow", "RightArrow" },
+  -- },
   -- modifier keys to combine with direction_keys
   modifiers = {
     move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
